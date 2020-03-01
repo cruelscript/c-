7fa8d8a9f87abbd83e28c87f9abcb1a43a6bf8c7
Nikita Kubov <kstart180@gmail.com>
2020-03-01 19:20:32 +0300

    members of the rectangle_t structure are fixed, default costructors circle, rectangle are added

--- kubov.nikita/A1/circle.hpp
+++ kubov.nikita/A1/circle.hpp
@@ -7,17 +7,20 @@
 class Circle : public Shape
 {
 public:
-  Circle(const point_t &position, const double radius);
+  Circle();
+  Circle(const point_t &center, const double radius);
   virtual double getArea() const override;
   virtual rectangle_t getFrameRect() const override;
   virtual void move(const point_t &positionNew) override;
   virtual void move(const double &deltaX, const double &deltaY) override;
   virtual void getInfo() const override;
+
 private:
-  point_t position_;
+  point_t center_;
   double radius_;
 
 };
+
 #endif
 
 
