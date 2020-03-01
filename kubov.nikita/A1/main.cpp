1df908706f0be3498deb8188f45b3c65b925c976
Nikita Kubov <kstart180@gmail.com>
2020-03-01 15:03:39 +0300

    Added lab A1 implementation

---
+++ kubov.nikita/A1/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "shape.hpp"
+#include "rectangle.hpp"
+#include "circle.hpp"
+
+void printInfoAboutFrameRect(Shape *figure)
+{
+  std::cout << "\nFrame rectangle " << std::endl;
+  std::cout << "Height = " << figure->getFrameRect().height << "\nWidth = " << figure->getFrameRect().width << "\n";
+  std::cout << "Center: " << "(" << figure->getFrameRect().position.x << ", "
+    << figure->getFrameRect().position.y << ")\n" << std::endl;
+}
+
+int main()
+{
+  point_t position = {1.1, 1.5};
+
+  Shape* figure = new Circle(position, 1.5);
+  figure->getInfo();
+
+  std::cout << "\nArea = " << figure->getArea() << std::endl;
+
+  printInfoAboutFrameRect(figure);
+
+  std::cout << "\nMove circle to (3.0, 4.0)" << std::endl;
+  point_t positionNew = { 3.0, 4.0 };
+  figure->move(positionNew);
+  figure->getInfo();
+
+  printInfoAboutFrameRect(figure);
+
+  figure = new Rectangle(position, 3.0, 4.0);
+  figure->getInfo();
+
+  std::cout << "\nArea = " << figure->getArea() << std::endl;
+
+  printInfoAboutFrameRect(figure);
+
+  std::cout << "Move rectangle on x: +5.1, y: -2.3\n" << std::endl;
+  figure->move(5.1, -2.3);
+  figure->getInfo();
+
+  printInfoAboutFrameRect(figure);
+
+  return 0;
+}
---
+++ kubov.nikita/A1/rectangle.hpp
@@ -0,0 +1,23 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include "shape.hpp"
+#include <iostream>
+
+class Rectangle : public Shape
+{
+public:
+  Rectangle(const point_t &position, const double width, const double height);
+  virtual double getArea() const override;
+  virtual rectangle_t getFrameRect() const override;
+  virtual void move(const point_t &positionNew) override;
+  virtual void move(const double &deltaX, const double &deltaY) override;
+  virtual void getInfo() const override;
+
+private:
+  point_t position_;
+  double width_;
+  double height_;
+
+};
+#endif
