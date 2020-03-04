a9064897c988f9add82ba621f9aa0c5670ed0cf4
Misha <mishakonov01@mail.ru>
2020-03-04 20:51:35 +0300

    A1 init commit

---
+++ A1/Rectangle.cpp
@@ -0,0 +1,30 @@
+#include "Rectangle.hpp"
+#include <cassert>
+
+Rectangle::Rectangle(const rectangle_t &rectangle) :
+        rectangle_(rectangle)
+{
+  assert(rectangle_.width_ > 0.0);
+  assert(rectangle_.height_ > 0.0);
+}
+
+void Rectangle::move(double dx, double dy)
+{
+  rectangle_.pos_.x_ += dx;
+  rectangle_.pos_.y_ += dy;
+}
+
+double Rectangle::getArea() const
+{
+  return rectangle_.width_ * rectangle_.height_;
+}
+
+void Rectangle::move(const point_t &point)
+{
+  rectangle_.pos_ = point;
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  return rectangle_;
+}
---
+++ A1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Rectangle.hpp"
+#include "Circle.hpp"
+
+inline void print(const Shape *shape)
+{
+  std::cout << "Shape: " << typeid(*shape).name() << "!!!" << "\n";
+  std::cout << "Frame rectangle(width, height): (" << shape->getFrameRect().width_
+            << ", " << shape->getFrameRect().height_ << ")" << "\n";
+  std::cout << "Frame rectangle position(x, y) : (" << shape->getFrameRect().pos_.x_ << ", "
+            << shape->getFrameRect().pos_.y_ << ")" << "\n";
+  std::cout << "Area of shape: " << shape->getArea() << "\n";
+}
+
+int main()
+{
+  Shape *rectangle = new Rectangle({2.28, 3.32, {0.1, 0.0}});
+  Shape *circle = new Circle(8.9,{1.1, 3.2});
+  Shape *shapes[2] = {rectangle, circle};
+
+  for (Shape *shape: shapes)
+  {
+    print(shape);
+    std::cout << "\n";
+    shape->move(1.1, 2.28);
+    print(shape);
+    std::cout << "\n";
+    shape->move({1.0, 1.0});
+    print(shape);
+    std::cout << "---------------\n";
+  }
+  return 0;
+}
