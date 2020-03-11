990471493888136aa3fd074dd863da4d84c5d3e0
Nikita Kubov <kstart180@gmail.com>
2020-03-11 18:38:35 +0300

    Added lab A2 implementation

---
+++ kubov.nikita/A2/circle.hpp
@@ -0,0 +1,29 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include "shape.hpp"
+
+namespace kubov
+{
+
+class Circle : public kubov::Shape
+{
+public:
+  Circle(const point_t &center, const double radius);
+  virtual double getArea() const override;
+  virtual rectangle_t getFrameRect() const override;
+  virtual void move(const point_t &positionNew) override;
+  virtual void move(const double &deltaX, const double &deltaY) override;
+  virtual void print() const override;
+  virtual void scale(const double coefficient) override;
+
+private:
+  point_t center_;
+  double radius_;
+
+};
+
+}
+#endif
+
+
---
+++ kubov.nikita/A2/shape.hpp
@@ -0,0 +1,24 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include "base-types.hpp"
+
+namespace kubov
+{
+
+class Shape
+{
+public:
+  virtual ~Shape() = default;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(const point_t &positionNew) = 0;
+  virtual void move(const double &deltaX, const double &deltaY) = 0;
+  virtual void print() const = 0;
+  virtual void scale(const double coefficient) = 0;
+};
+
+}
+#endif
+
+
