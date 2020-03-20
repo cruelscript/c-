3bf0512305c67b25fafa0da40a3375541a92d3b8
Prishchepa Ilya <i-o-pr@ya.ru>
2020-03-20 23:27:44 +0300

    Make get* methods of class Shape constant
    Make methods getArea() and getFrameRect() from class Shape constant.

--- prishchepa.ilya/A1/shape.hpp
+++ prishchepa.ilya/A1/shape.hpp
@@ -7,8 +7,8 @@ class Shape
 {
 public:
   virtual ~Shape() = default;
-  virtual double getArea() = 0;
-  virtual rectangle_t getFrameRect() = 0;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
   virtual void move(const point_t & destination) = 0;
   virtual void move(double deltaX, double deltaY) = 0;
 
