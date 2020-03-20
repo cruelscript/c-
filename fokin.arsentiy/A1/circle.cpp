7e84751e817cc17de20fb7201bfbd4cc273d64da
Arsentiy Fokin <puncheglaz@gmail.com>
2020-03-20 23:38:26 +0500

    First commit

---
+++ fokin.arsentiy/A1/circle.cpp
@@ -0,0 +1,44 @@
+#include "circle.hpp"
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <cassert>
+
+Circle::Circle(const point_t& center, const double radius) :
+    center_{ center.x, center.y }, 
+    radius_{ radius }
+{
+    assert(radius > 0 && "Error! Radius must be a positive value. Exiting...\n");
+}
+
+double Circle::getArea() const
+{
+    return radius_ * radius_ * M_PI;
+}
+
+rectangle_t Circle::getFrameRect() const
+{
+    rectangle_t rect_{ radius_ * 2, radius_ * 2, center_ };
+    return rect_;
+}
+
+point_t Circle::getCenter() const
+{
+    return center_;
+}
+
+double Circle::getRadius() const
+{
+    return radius_;
+}
+
+void Circle::move(const point_t& newCenter)
+{
+    center_ = newCenter;
+}
+
+void Circle::move(const double dx, const double dy)
+{
+    center_.x += dx;
+    center_.y += dy;
+}
---
+++ fokin.arsentiy/A1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "rectangle.hpp"
+#include "circle.hpp"
+
+int main()
+{
+    Rectangle testRectangle{ 4.0, 5.0, { 2.6, -1.4} };
+    std::cout << "Rectangle's area: " << testRectangle.getArea() << "\n";
+    rectangle_t frameRectangle = testRectangle.getFrameRect();
+    std::cout << "Height of test and frame rectangle: " << frameRectangle.height << "\n";
+    std::cout << "Width of test and frame rectangle: " << frameRectangle.width << "\n";
+    std::cout << "Center of test and frame rectangle: (" << frameRectangle.pos.x << ", " << frameRectangle.pos.y << ")\n";
+    testRectangle.move({ -5.1, 9.7 });
+    testRectangle.move(5.1, -9.7);
+    frameRectangle = testRectangle.getFrameRect();
+    std::cout << "New center of test and frame rectangle: (" << frameRectangle.pos.x << ", " << frameRectangle.pos.y << ")\n\n";
+
+    Circle testCircle{ { 5.2, -7.5 }, 4.0 };
+    std::cout << "Circle's area: " << testCircle.getArea() << "\n";
+    std::cout << "Circle's radius: " << testCircle.getRadius() << "\n";
+    frameRectangle = testCircle.getFrameRect();
+    std::cout << "Height of frame rectangle: " << frameRectangle.height << "\n";
+    std::cout << "Width of frame rectangle: " << frameRectangle.width << "\n";
+    std::cout << "Center of circle and frame rectangle: (" << frameRectangle.pos.x << ", " << frameRectangle.pos.y << ")\n";
+    testCircle.move({ 6.0, -1.6 });
+    testCircle.move(-6.0, 1.6);
+    std::cout << "New center of circle: (" << testCircle.getCenter().x << ", " << testCircle.getCenter().y << ")\n";
+
+    Rectangle incorrectRectangle{ 4.0, -5.0, { 2.6, -1.4} };
+    Circle incorrectCircle{ { 5.2, -7.5 }, -4.0 };
+
+    return 0;
+}
