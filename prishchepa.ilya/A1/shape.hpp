5709b878eede7bd65783f72f4950e97689122ef1
Prishchepa Ilya <i-o-pr@ya.ru>
2020-03-20 23:40:41 +0300

    Add getPosition() method to class Shape

--- prishchepa.ilya/A1/shape.hpp
+++ prishchepa.ilya/A1/shape.hpp
@@ -11,9 +11,15 @@ public:
   virtual rectangle_t getFrameRect() const = 0;
   virtual void move(const point_t & destination) = 0;
   virtual void move(double deltaX, double deltaY) = 0;
+  point_t getPosition() const;
 
 private:
   point_t pos_;
+};
+
+point_t Shape::getPosition() const
+{
+  return pos_;
 }
 
 #endif
