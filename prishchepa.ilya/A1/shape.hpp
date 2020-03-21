8c8cf922bdde37ba7f13ef9128e509424c2f1a80
Prishchepa Ilya <i-o-pr@ya.ru>
2020-03-21 15:02:15 +0300

    Improve class Shape
    Add constructor;
    Define move methods;
    Define getPosition method;
    Make pos_ filed protected instead of private.

--- prishchepa.ilya/A1/shape.hpp
+++ prishchepa.ilya/A1/shape.hpp
@@ -6,20 +6,16 @@
 class Shape
 {
 public:
+  Shape(const point_t & position);
   virtual ~Shape() = default;
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
-  virtual void move(const point_t & destination) = 0;
-  virtual void move(double deltaX, double deltaY) = 0;
+  virtual void move(const point_t & destination);
+  virtual void move(double deltaX, double deltaY);
   point_t getPosition() const;
 
-private:
+protected:
   point_t pos_;
 };
-
-point_t Shape::getPosition() const
-{
-  return pos_;
-}
 
 #endif
