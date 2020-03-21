3a3453be3de59b3e87d72049f002d8d866ebf7c6
Prishchepa Ilya <i-o-pr@ya.ru>
2020-03-21 15:54:31 +0300

    Implement class Rectangle

---
+++ prishchepa.ilya/A1/rectangle.cpp
@@ -0,0 +1,32 @@
+#include "rectangle.hpp"
+
+#include <cassert>
+
+Rectangle::Rectangle(const point_t & position, double width, double height) :
+  Shape(position),
+  width_(width),
+  height_(height)
+{
+  assert(width > 0.0);
+  assert(height > 0.0);
+}
+
+double Rectangle::getArea() const
+{
+  return width_ * height_;
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  return rectangle_t{width_, height_, pos_};
+}
+
+double Rectangle::getWidth() const
+{
+  return width_;
+}
+
+double Rectangle::getHeight() const
+{
+  return height_;
+}
