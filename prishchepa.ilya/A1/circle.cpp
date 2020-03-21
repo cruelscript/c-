3477f0b9372553e256c3639ac35514130fb55c00
Prishchepa Ilya <i-o-pr@ya.ru>
2020-03-21 16:08:59 +0300

    Implement class Circle

---
+++ prishchepa.ilya/A1/circle.cpp
@@ -0,0 +1,28 @@
+#include "circle.hpp"
+
+#include <cassert>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+Circle::Circle(const point_t & position, double radius) :
+  Shape(position),
+  radius_(radius)
+{
+  assert(radius > 0.0);
+}
+
+double Circle::getArea() const
+{
+  return M_PI * radius_ * radius_;
+}
+
+rectangle_t Circle::getFrameRect() const
+{
+  return rectangle_t{radius_ * 2.0, radius_ * 2.0, pos_};
+}
+
+double Circle::getRadius() const
+{
+  return radius_;
+}
---
+++ prishchepa.ilya/A1/circle.hpp
@@ -0,0 +1,18 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include "shape.hpp"
+
+class Circle : public Shape
+{
+public:
+  Circle(const point_t & position, double radius);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  double getRadius() const;
+
+private:
+  double radius_;
+};
+
+#endif
