d13d697be6088f4d3019149875033f6b56ac9e1d
KrGolovin <57076115+KrGolovin@users.noreply.github.com>
2020-03-26 23:12:37 +0300

    the correct tests are written

--- golovin.kirill/A1/main.cpp
+++ golovin.kirill/A1/main.cpp
@@ -16,19 +16,12 @@ int main()
               << figures[i]->getFrameRect().pos.y << ")\n";
     std::cout << "Width frame Rectangle of " << nameObj << " is " << figures[i]->getFrameRect().width
               << " and height is " << figures[i]->getFrameRect().height << "\n\n";
+    figures[i]->move({1, 2});
+    figures[i]->move(1, 2);
+    std::cout << "Position " << std::string(typeid(*figures[i]).name()).substr(1) << "after moving in (1, 2) is ("
+              << figures[i]->getPos().x << "; " << figures[i]->getPos().y << ")\n";
+    std::cout << "Position " << std::string(typeid(*figures[i]).name()).substr(1) << "after moving to offset (1, 2) is ("
+              << figures[i]->getPos().x << "; " << figures[i]->getPos().y << ")\n\n";
   }
-
-  std::cout << "Position of circle is (" << figures[0]->getPos().x << "; " << figures[0]->getPos().y << ")\n";
-
-  figures[0]->move({1, 2});
-  figures[1]->move(1, 2);
-  std::cout << "Position " << std::string(typeid(*figures[0]).name()).substr(1) << "after moving in (2, 3) is ("
-            << figures[0]->getPos().x << "; " << figures[0]->getPos().y << ")\n\n";
-
-  std::cout << "Position of rectangle is (" << figures[1]->getPos().x << "; " << figures[1]->getPos().y << ")\n";
-  std::cout << "Moving the sides of first rectangle by 2 in width and 3 in height" << "\n";
-
-  std::cout << "Position " << std::string(typeid(*figures[1]).name()).substr(1) << "after moving to offset (2, 3) is ("
-            << figures[1]->getPos().x << "; " << figures[1]->getPos().y << ")\n";
   return 0;
 }
