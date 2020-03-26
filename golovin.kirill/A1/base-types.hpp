e71498a893be81e13a6942303cc519bdbb8af9a5
KrGolovin <57076115+KrGolovin@users.noreply.github.com>
2020-03-26 10:22:41 +0300

    fixed bases classes to structs

--- golovin.kirill/A1/base-types.hpp
+++ golovin.kirill/A1/base-types.hpp
@@ -1,16 +1,14 @@
 #ifndef A1_BASE_TYPES_HPP
 #define A1_BASE_TYPES_HPP
 
-class point_t
+struct point_t
 {
-public:
   double x;
   double y;
 };
 
-class rectangle_t
+struct rectangle_t
 {
-public:
   double width;
   double height;
   point_t pos;
