41ca66c0e0b132375ea6abd5564dca630373e331
sviridov.ivan <artem.sviridov.99@list.ru>
2020-03-27 03:49:46 +0300

    correct tabulation

--- sviridov.ivan/A1/shape.hpp
+++ sviridov.ivan/A1/shape.hpp
@@ -6,13 +6,13 @@
 class  Shape
 {
 public:
-	virtual ~Shape() = default;
-	virtual double getArea() const = 0;
-	virtual rectangle_t getFrameRect() const = 0;
-	virtual void move(const double dx, const double dy) = 0;
-	virtual void move(const point_t& point) = 0;
-	virtual point_t getCentre() const = 0;
-	virtual void show_frame(Shape *figure) const = 0;
-	virtual void show_position(Shape* figure) const = 0;
+  virtual ~Shape() = default;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(const double dx, const double dy) = 0;
+  virtual void move(const point_t& point) = 0;
+  virtual point_t getCentre() const = 0;
+  virtual void show_frame(Shape *figure) const = 0;
+  virtual void show_position(Shape* figure) const = 0;
 };
 #endif
