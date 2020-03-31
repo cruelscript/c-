c778ac4afc1e810bf7da7d6178390a2c01d6a006
sviridov.ivan <artem.sviridov.99@list.ru>
2020-04-01 02:37:28 +0300

    corrrected formalization
    Please enter the commit message for your changes. Lines starting

---
+++ sviridov.ivan/A1/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "rectangle.hpp"
+#include "circle.hpp"
+
+void printFrameInfo(Shape* figure)
+{
+  std::cout << "Frame height: " << figure->getFrameRect().height << "\n"
+    << "Frame width: " << figure->getFrameRect().width << "\n"
+    << "Frame centre: (" << figure->getFrameRect().pos.x << ", " << figure->getFrameRect().pos.y << ")\n";
+}
+
+int main()
+{
+  Rectangle rectangleTest({6.7, 2.4}, 10.6, 5.1);
+  Shape* figure = &rectangleTest;
+  std::cout << "Rectangle's area: " << figure->getArea() <<  "\n\n";
+  printFrameInfo(figure);
+  figure->move(4.6, -6.7);
+  figure->printCentre();
+  figure->move({-7.4, 5.9});
+  figure->printCentre();
+
+  Circle circleTest({1.4, 12.3}, 6.7);
+  figure = &circleTest;
+  std::cout << "\nCircle area: " << figure->getArea() <<  "\n\n";
+  printFrameInfo(figure);
+  figure->move(11.6, -20.4);
+  figure->printCentre();
+  figure->move({35.6, -13.9});
+  figure->printCentre();
+
+  return 0;
+}
---
+++ sviridov.ivan/A1/rectangle.cpp
@@ -0,0 +1,37 @@
+#include "rectangle.hpp"
+#include <iostream>
+#include <cassert>
+
+Rectangle::Rectangle(const point_t& centre, double width, double height) :
+  width_(width),
+  height_(height),
+  centre_(centre)
+{
+  assert((width > 0) && (height > 0));
+}
+
+void Rectangle::move(const point_t& point)
+{
+  centre_ = point;
+}
+
+void Rectangle::move(double dx, double dy)
+{
+  centre_.x += dx;
+  centre_.y += dy;
+}
+
+double Rectangle::getArea() const
+{
+  return width_ * height_;
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  return {centre_, width_, height_};
+}
+
+void Rectangle::printCentre() const
+{
+  std::cout << "Rectangle centre: (" << centre_.x << ", " << centre_.y << ")\n";
+}
