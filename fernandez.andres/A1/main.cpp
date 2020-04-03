92bd7475d3de7765b606e8556adde9b7c74e7869
andyferfl <andviolin7@gmail.com>
2020-04-03 23:42:11 +0300

    added test for both types of move

--- fernandez.andres/A1/main.cpp
+++ fernandez.andres/A1/main.cpp
@@ -9,9 +9,9 @@ int main()
   Rectangle rect(point, 30, 40);
   Circle circle({ 100, 150 }, 50);
   Shape* array[2] = { &rect, &circle };
-  array[0]->print();
+  array[0]->print(std::cout);
   std::cout << "Area = " << array[0]->getArea() << '\n';
-  array[1]->print();
+  array[1]->print(std::cout);
   std::cout << "Area = " << array[1]->getArea() << '\n';
 
   std::cout << "Frame Rects" << '\n';
@@ -19,22 +19,29 @@ int main()
       << ") width = " << array[0]->getFrameRect().width << " height = " << array[0]->getFrameRect().height << '\n';
   std::cout << "for circle" << " center (" << array[1]->getFrameRect().pos.x << ", " << array[1]->getFrameRect().pos.y
       << ") width = " << array[1]->getFrameRect().width << " height = " << array[1]->getFrameRect().height << '\n';
+  std::cout << "    --------   *   ---------" << '\n';
 
-  std::cout << "Move rect to (25, 25)" << '\n';
   point = { 25, 25 };
-  array[0]->move(point);
-  array[0]->print();
-  std::cout << "Area = " << array[0]->getArea() << '\n';
-  std::cout << "Frame rect of rect" << " center (" << array[0]->getFrameRect().pos.x << ", "
-      << array[0]->getFrameRect().pos.y << ") width = " << array[0]->getFrameRect().width
-          << " height = " << array[0]->getFrameRect().height << '\n';
 
-  std::cout << "Move circle +5, -7" << '\n';
-  array[1]->move(5, -7);
-  array[1]->print();
-  std::cout << "Area = " << array[1]->getArea() << '\n';
-  std::cout << "Frame rect of  circle" << " center (" << array[1]->getFrameRect().pos.x << ", "
-      << array[1]->getFrameRect().pos.y << ") width = " << array[1]->getFrameRect().width
-          << " height = " << array[1]->getFrameRect().height << '\n';
+  for (int i = 0; i < 2; i++)
+  {
+    std::cout << "Move to (25, 25)" << '\n';
+    array[i]->move(point);
+    array[i]->print(std::cout);
+    std::cout << "Area = " << array[i]->getArea() << '\n';
+    std::cout << "Frame rect " << " center (" << array[i]->getFrameRect().pos.x << ", "
+      << array[i]->getFrameRect().pos.y << ") width = " << array[0]->getFrameRect().width
+      << " height = " << array[i]->getFrameRect().height << '\n';
+
+    std::cout << "Move +5, -7" << '\n';
+    array[i]->move(5, -7);
+    array[i]->print(std::cout);
+    std::cout << "Area = " << array[i]->getArea() << '\n';
+    std::cout << "Frame rect " << " center (" << array[i]->getFrameRect().pos.x << ", "
+      << array[i]->getFrameRect().pos.y << ") width = " << array[i]->getFrameRect().width
+      << " height = " << array[i]->getFrameRect().height << '\n';
+
+    std::cout << "    --------   *   ---------" << '\n';
+  }
   return 0;
 }
