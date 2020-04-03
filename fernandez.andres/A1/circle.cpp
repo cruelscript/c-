b7edf72219a007b9e6cb8fdeb9ccad05bd8aef00
andyferfl <andviolin7@gmail.com>
2020-04-03 23:44:39 +0300

    corrected style rule 39

--- fernandez.andres/A1/circle.cpp
+++ fernandez.andres/A1/circle.cpp
@@ -1,7 +1,8 @@
-#define _USE_MATH_DEFINES
 #include "circle.hpp"
-#include <cmath>
+#define _USE_MATH_DEFINES
 #include <cassert>
+#include <cmath>
+#include <ostream>
 
 Circle::Circle(const point_t& position, const double radius) :
   Shape(position),
@@ -20,9 +21,9 @@ rectangle_t Circle::getFrameRect() const
   return { position_, 2 * radius_, 2 * radius_ };
 }
 
-void Circle::print() const
+void Circle::print(std::ostream & out) const
 {
-  std::cout << "Circle.\n";
-  std::cout << "Position: x = " << position_.x << ", y = " << position_.y << '\n';
-  std::cout << "Radius = " << radius_ << '\n';
+  out << "Circle.\n";
+  out << "Position: x = " << position_.x << ", y = " << position_.y << '\n';
+  out << "Radius = " << radius_ << '\n';
 }
