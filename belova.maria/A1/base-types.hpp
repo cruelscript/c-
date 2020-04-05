e183e570c6ac0690eaeb78a8df8a98523e1f8721
Belova Maria <masha.belova.01@mail.ru>
2020-04-05 10:05:22 +0300

    Deleted friend output operator

--- belova.maria/A1/base-types.hpp
+++ belova.maria/A1/base-types.hpp
@@ -1,18 +1,10 @@
 #ifndef BASE_TYPES_HPP
 #define BASE_TYPES_HPP
 
-#include <iostream>
-
 struct point_t
 {
   double x;
   double y;
-
-  friend std::ostream& operator<<(std::ostream &out, const point_t &point)
-  {
-    out << '(' << point.x << ", " << point.y << ')';
-    return out;
-  }
 };
 
 struct rectangle_t
