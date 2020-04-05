675097d2ea72c1e2c95c8615ae6e6051e25d489f
Jesenija Gromova <esenijagromova0822@gmail.com>
2020-04-05 21:31:11 +0300

    Исправлена ситуация с утечкой памяти в main.cpp.

--- gromova.esenija/A1/main.cpp
+++ gromova.esenija/A1/main.cpp
@@ -5,38 +5,37 @@
 
 int main()
 {
-  Shape* rectangle = new Rectangle(6.8, 4.2, { -3, -9 });
-  Shape* circle = new Circle(5.7, { 2.5, 8 });
+  Rectangle rectangle(6.8, 4.2, { -3, -9 });
+  Circle circle(5.7, { 2.5, 8 });
+  Shape* pRectangle = &rectangle;
+  Shape* pCircle = &circle;
 
-  std::cout << "Rectangle area: " << rectangle->getArea() << "\n";
-  std::cout << "Circle area: " << circle->getArea() << "\n\n";
+  std::cout << "Rectangle area: " << pRectangle->getArea() << "\n";
+  std::cout << "Circle area: " << pCircle->getArea() << "\n\n";
 
-  std::cout << "Frame rectangle parameters for rectangle:\nHeight: " << rectangle->getFrameRect().height
-      << "\nWidth: " << rectangle->getFrameRect().width << "\nCenter at the point: ("
-          << rectangle->getFrameRect().pos.x << "; " << rectangle->getFrameRect().pos.y << ")\n\n";
+  std::cout << "Frame rectangle parameters for rectangle:\nHeight: " << pRectangle->getFrameRect().height
+      << "\nWidth: " << pRectangle->getFrameRect().width << "\nCenter at the point: ("
+          << pRectangle->getFrameRect().pos.x << "; " << pRectangle->getFrameRect().pos.y << ")\n\n";
 
-  std::cout << "Frame rectangle parameters for circle:\nHeight: " << circle->getFrameRect().height
-      << "\nWidth: " << circle->getFrameRect().width << "\nCenter at the point: ("
-          << circle->getFrameRect().pos.x << "; " << circle->getFrameRect().pos.y << ")\n\n";
+  std::cout << "Frame rectangle parameters for circle:\nHeight: " << pCircle->getFrameRect().height
+      << "\nWidth: " << pCircle->getFrameRect().width << "\nCenter at the point: ("
+          << pCircle->getFrameRect().pos.x << "; " << pCircle->getFrameRect().pos.y << ")\n\n";
 
   std::cout << "Move a rectangle to the point (2; 6.3)\n";
-  rectangle->move({ 2, 6.3 });
-  std::cout << "Center at the point: (" << rectangle->getFrameRect().pos.x << "; " << rectangle->getFrameRect().pos.y << ")\n\n";
+  pRectangle->move({ 2, 6.3 });
+  std::cout << "Center at the point: (" << pRectangle->getFrameRect().pos.x << "; " << pRectangle->getFrameRect().pos.y << ")\n\n";
 
   std::cout << "Move a circle to the point (-5.9; 2)\n";
-  circle->move({ -5.9, 2 });
-  std::cout << "Center at the point: (" << circle->getFrameRect().pos.x << "; " << circle->getFrameRect().pos.y << ")\n\n";
+  pCircle->move({ -5.9, 2 });
+  std::cout << "Center at the point: (" << pCircle->getFrameRect().pos.x << "; " << pCircle->getFrameRect().pos.y << ")\n\n";
 
   std::cout << "Move the rectangle along the X axis by 3 and along the Y axis by -4\n";
-  rectangle->move(3, -4);
-  std::cout << "Center at the point: (" << rectangle->getFrameRect().pos.x << "; " << rectangle->getFrameRect().pos.y << ")\n\n";
+  pRectangle->move(3, -4);
+  std::cout << "Center at the point: (" << pRectangle->getFrameRect().pos.x << "; " << pRectangle->getFrameRect().pos.y << ")\n\n";
 
   std::cout << "Move the circle along the X axis by -2 and the Y axis by 6.7\n";
-  circle->move(-2, 6.7);
-  std::cout << "Center at the point: (" << circle->getFrameRect().pos.x << "; " << circle->getFrameRect().pos.y << ")\n\n";
-
-  delete rectangle;
-  delete circle;
+  pCircle->move(-2, 6.7);
+  std::cout << "Center at the point: (" << pCircle->getFrameRect().pos.x << "; " << pCircle->getFrameRect().pos.y << ")\n\n";
 
   return 0;
 }
