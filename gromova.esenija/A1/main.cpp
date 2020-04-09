a9a91bb1f9481340b2f3947c954680fc38cfd0e8
Jesenija Gromova <esenijagromova0822@gmail.com>
2020-04-09 18:21:45 +0300

    Сокращение похожего кода с помощью цикла в main.cpp

--- gromova.esenija/A1/main.cpp
+++ gromova.esenija/A1/main.cpp
@@ -7,35 +7,24 @@ int main()
 {
   Rectangle rectangle(6.8, 4.2, { -3, -9 });
   Circle circle(5.7, { 2.5, 8 });
-  Shape* pRectangle = &rectangle;
-  Shape* pCircle = &circle;
+  const int SIZE = 2;
+  Shape* shapes[SIZE] = { &rectangle, &circle };
 
-  std::cout << "Rectangle area: " << pRectangle->getArea() << "\n";
-  std::cout << "Circle area: " << pCircle->getArea() << "\n\n";
-
-  std::cout << "Frame rectangle parameters for rectangle:\nHeight: " << pRectangle->getFrameRect().height
-      << "\nWidth: " << pRectangle->getFrameRect().width << "\nCenter at the point: ("
-          << pRectangle->getFrameRect().pos.x << "; " << pRectangle->getFrameRect().pos.y << ")\n\n";
-
-  std::cout << "Frame rectangle parameters for circle:\nHeight: " << pCircle->getFrameRect().height
-      << "\nWidth: " << pCircle->getFrameRect().width << "\nCenter at the point: ("
-          << pCircle->getFrameRect().pos.x << "; " << pCircle->getFrameRect().pos.y << ")\n\n";
-
-  std::cout << "Move a rectangle to the point (2; 6.3)\n";
-  pRectangle->move({ 2, 6.3 });
-  std::cout << "Center at the point: (" << pRectangle->getFrameRect().pos.x << "; " << pRectangle->getFrameRect().pos.y << ")\n\n";
-
-  std::cout << "Move a circle to the point (-5.9; 2)\n";
-  pCircle->move({ -5.9, 2 });
-  std::cout << "Center at the point: (" << pCircle->getFrameRect().pos.x << "; " << pCircle->getFrameRect().pos.y << ")\n\n";
-
-  std::cout << "Move the rectangle along the X axis by 3 and along the Y axis by -4\n";
-  pRectangle->move(3, -4);
-  std::cout << "Center at the point: (" << pRectangle->getFrameRect().pos.x << "; " << pRectangle->getFrameRect().pos.y << ")\n\n";
-
-  std::cout << "Move the circle along the X axis by -2 and the Y axis by 6.7\n";
-  pCircle->move(-2, 6.7);
-  std::cout << "Center at the point: (" << pCircle->getFrameRect().pos.x << "; " << pCircle->getFrameRect().pos.y << ")\n\n";
+  for (int i = 0; i < SIZE; ++i)
+  {
+    (i == 0) ? std::cout << "-Rectangle-\n\n" : std::cout << "-Circle-\n\n";
+    std::cout << "Area: " << shapes[i]->getArea() << "\n";
+    std::cout << "Frame rectangle parameters for circle:\nHeight: " << shapes[i]->getFrameRect().height
+        << "\nWidth: " << shapes[i]->getFrameRect().width << "\nCenter at the point: (" << shapes[i]->getFrameRect().pos.x
+            << "; " << shapes[i]->getFrameRect().pos.y << ")\n";
+    std::cout << "Move this shape to the point (4.7; 6.3)\nAfter moving ";
+    shapes[i]->move({ 4.7, 6.3 });
+    std::cout << "center at the point: (" << shapes[i]->getFrameRect().pos.x << "; " << shapes[i]->getFrameRect().pos.y << ")\n";
+    std::cout << "Move this shape along the X axis by 3 and along the Y axis by -4\nAfter moving ";
+    shapes[i]->move(3, -4);
+    std::cout << "center at the point: (" << shapes[i]->getFrameRect().pos.x << "; " << shapes[i]->getFrameRect().pos.y << ")\n";
+    std::cout << "\n";
+  }
 
   return 0;
 }
