86c98d89cfa2729e071992be6975ad48e3b1789e
feddoa <feda.gyrlov@yandex.ru>
2020-04-10 14:56:13 +0300

    unified

--- gyrlov.fedor/A1/main.cpp
+++ gyrlov.fedor/A1/main.cpp
@@ -14,22 +14,22 @@ int main()
     std::cout << shapeType << '\n';
 
     std::cout << "Frame position (x, y): (" << figure[i]->getFrameRect().pos.x
-              << ", " << figure[i]->getFrameRect().pos.y << ")\n"
-    << "Frame width of figure is " << figure[i]->getFrameRect().width << "\n"
-    << "Frame height of figure is " << figure[i]->getFrameRect().height << "\n\n";
+      << ", " << figure[i]->getFrameRect().pos.y << ")\n"
+      << "Frame width of figure is " << figure[i]->getFrameRect().width << "\n"
+      << "Frame height of figure is " << figure[i]->getFrameRect().height << "\n\n";
 
     std::cout << "The figure's area is " << figure[i]->getArea() << "\n\n";
 
     figure[i]->move(3.4, 6.0);
 
     std::cout << "Frame position after moving figure along: (" << figure[i]->getFrameRect().pos.x
-    << ", " << figure[i]->getFrameRect().pos.y << ")\n";
+      << ", " << figure[i]->getFrameRect().pos.y << ")\n";
 
     figure[i]->move({2.4, -5.5});
 
     std::cout << "Frame position after moving figure to a new point: ("
-    << figure[i]->getFrameRect().pos.x << ", " << figure[i]->getFrameRect().pos.y
-    << ")\n\n";
+      << figure[i]->getFrameRect().pos.x << ", " << figure[i]->getFrameRect().pos.y
+      << ")\n\n";
   }
   return 0;
 }
