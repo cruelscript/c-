9bf88fd5146e1f70774199ba9e3c170ce5efbbd5
andyferfl <andviolin7@gmail.com>
2020-04-16 02:46:14 +0300

    created function printStatus

--- fernandez.andres/A1/main.cpp
+++ fernandez.andres/A1/main.cpp
@@ -3,6 +3,14 @@
 #include "circle.hpp"
 #include "rectangle.hpp"
 
+void printStatus(const double& area, const rectangle_t& rect)
+{
+  std::cout << "Area = " << area << '\n';
+  std::cout << "Frame Rects" << '\n';
+  std::cout << "center (" << rect.pos.x << ", " << rect.pos.y
+    << ") width = " << rect.width << " height = " << rect.height << '\n';
+}
+
 int main()
 {
   point_t point = { 32, 25 };
@@ -15,27 +23,17 @@ int main()
   for (int i = 0; i < 2; i++)
   {
     array[i]->print(std::cout);
-    std::cout << "Area = " << array[i]->getArea() << '\n';
-
-    std::cout << "Frame Rects" << '\n';
-    std::cout << " center (" << array[i]->getFrameRect().pos.x << ", " << array[i]->getFrameRect().pos.y
-      << ") width = " << array[i]->getFrameRect().width << " height = " << array[i]->getFrameRect().height << '\n';
+    printStatus(array[i]->getArea(), array[i]->getFrameRect());
 
     std::cout << "Move to (25, 25)" << '\n';
     array[i]->move(point);
     array[i]->print(std::cout);
-    std::cout << "Area = " << array[i]->getArea() << '\n';
-    std::cout << "Frame rect " << " center (" << array[i]->getFrameRect().pos.x << ", "
-      << array[i]->getFrameRect().pos.y << ") width = " << array[0]->getFrameRect().width
-      << " height = " << array[i]->getFrameRect().height << '\n';
+    printStatus(array[i]->getArea(), array[i]->getFrameRect());
 
     std::cout << "Move +5, -7" << '\n';
     array[i]->move(5, -7);
     array[i]->print(std::cout);
-    std::cout << "Area = " << array[i]->getArea() << '\n';
-    std::cout << "Frame rect " << " center (" << array[i]->getFrameRect().pos.x << ", "
-      << array[i]->getFrameRect().pos.y << ") width = " << array[i]->getFrameRect().width
-      << " height = " << array[i]->getFrameRect().height << '\n';
+    printStatus(array[i]->getArea(), array[i]->getFrameRect());
 
     std::cout << "    --------   *   ---------" << '\n';
   }
