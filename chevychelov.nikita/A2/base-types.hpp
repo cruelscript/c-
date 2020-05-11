7a08f2592dbcc568fe30bdd99b32f70d5a242b4f
Nikita Chevychelov <nikita222229@mail.ru>
2020-05-11 13:32:16 +0000

    A2

---
+++ chevychelov.nikita/A2/base-types.hpp
@@ -0,0 +1,19 @@
+#ifndef BASE_TYPES_HPP_
+#define BASE_TYPES_HPP_
+
+namespace chevychelov
+{
+  struct point_t
+  {
+    double x;
+    double y;
+  };
+
+  struct rectangle_t
+  {
+    double width;
+    double height;
+    point_t pos;
+  };
+}
+#endif // !BASE_TYPES_HPP_
---
+++ chevychelov.nikita/A2/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "rectangle.hpp"
+#include "circle.hpp"
+
+int main()
+{
+  chevychelov::point_t mid = {0.0, 0.0};
+  chevychelov::Rectangle rect(10.10, 12.12, mid);
+  chevychelov::Circle circle(5.12, mid);
+  chevychelov::Shape* figures[2] = {&rect, &circle};
+  for (chevychelov::Shape* shape : figures)
+  {
+    std::cout << "Your figures: ";
+    shape->print(std::cout);
+    std::cout << " Area of figure: " << shape->getArea();
+    std::cout << " \n Frame rectangle: " << " Width: " << shape->getFrameRect().width
+      << " Height: " << rect.getFrameRect().height << " X: " << shape->getFrameRect().pos.x
+      << " Y: " << shape->getFrameRect().pos.y;
+    shape->move(11.3, 7.0);
+    std::cout << " \nYour figure after move: ";
+    shape->print(std::cout);
+    shape->move(mid);
+    std::cout << "Your figure after move back: ";
+    shape->print(std::cout);
+    std::cout << "Your figure after scale: ";
+    shape->scale(2.15);
+    shape->print(std::cout);
+  }
+  return 0;
+};
